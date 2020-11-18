2c3aa5148dcd3e156d5287a374779848c7f2a236
Taylor Fahy <taylorfahy1337@gmail.com>
2020-11-18 21:17:43 +0000

    Added map

--- RaiseTheGameJame2020/Intermediate/Build/Win64/UE4Editor/Inc/RaiseTheGameJame2020/RaiseTheGameJame2020.init.gen.cpp
+++ RaiseTheGameJame2020/Intermediate/Build/Win64/UE4Editor/Inc/RaiseTheGameJame2020/RaiseTheGameJame2020.init.gen.cpp
@@ -21,18 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeRaiseTheGameJame2020_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-<<<<<<< HEAD
-<<<<<<< HEAD
-				0xFA773344,
-				0x476D8F57,
-=======
-				0xE8903AF0,
-				0xC3257138,
->>>>>>> KillAbility
-=======
-				0x1EAB47F0,
-				0xD77EEC93,
->>>>>>> Tasks
+				0x0F7B3A08,
+				0x71B1FE47,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
