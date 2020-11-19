9256d071d6c5855bf6bfda354fcb07dd2574f495
Rhiannon <p018236h@student.staffs.ac.uk>
2020-11-19 17:28:33 +0000

    Idk why it didnt work before but now it does

--- RaiseTheGameJame2020/Intermediate/Build/Win64/UE4Editor/Inc/RaiseTheGameJame2020/RaiseTheGameJame2020Character.generated.h
+++ RaiseTheGameJame2020/Intermediate/Build/Win64/UE4Editor/Inc/RaiseTheGameJame2020/RaiseTheGameJame2020Character.generated.h
@@ -16,13 +16,8 @@ struct FHitResult;
 #endif
 #define RAISETHEGAMEJAME2020_RaiseTheGameJame2020Character_generated_h
 
-<<<<<<< HEAD
-#define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_SPARSE_DATA
-#define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_RPC_WRAPPERS \
-=======
 #define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_SPARSE_DATA
 #define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_RPC_WRAPPERS \
->>>>>>> main
  \
 	DECLARE_FUNCTION(execOnGrabSphereEndOverlap); \
 	DECLARE_FUNCTION(execOnGrabSphereBeginOverlap); \
@@ -32,11 +27,7 @@ struct FHitResult;
 	DECLARE_FUNCTION(execAUpdate);
 
 
-<<<<<<< HEAD
-#define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_RPC_WRAPPERS_NO_PURE_DECLS \
-=======
 #define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_RPC_WRAPPERS_NO_PURE_DECLS \
->>>>>>> main
  \
 	DECLARE_FUNCTION(execOnGrabSphereEndOverlap); \
 	DECLARE_FUNCTION(execOnGrabSphereBeginOverlap); \
@@ -46,11 +37,7 @@ struct FHitResult;
 	DECLARE_FUNCTION(execAUpdate);
 
 
-<<<<<<< HEAD
-#define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_INCLASS_NO_PURE_DECLS \
-=======
 #define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_INCLASS_NO_PURE_DECLS \
->>>>>>> main
 private: \
 	static void StaticRegisterNativesARaiseTheGameJame2020Character(); \
 	friend struct Z_Construct_UClass_ARaiseTheGameJame2020Character_Statics; \
@@ -59,11 +46,7 @@ public: \
 	DECLARE_SERIALIZER(ARaiseTheGameJame2020Character)
 
 
-<<<<<<< HEAD
-#define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_INCLASS \
-=======
 #define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_INCLASS \
->>>>>>> main
 private: \
 	static void StaticRegisterNativesARaiseTheGameJame2020Character(); \
 	friend struct Z_Construct_UClass_ARaiseTheGameJame2020Character_Statics; \
@@ -72,11 +55,7 @@ public: \
 	DECLARE_SERIALIZER(ARaiseTheGameJame2020Character)
 
 
-<<<<<<< HEAD
-#define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_STANDARD_CONSTRUCTORS \
-=======
 #define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_STANDARD_CONSTRUCTORS \
->>>>>>> main
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API ARaiseTheGameJame2020Character(const FObjectInitializer& ObjectInitializer); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ARaiseTheGameJame2020Character) \
@@ -89,11 +68,7 @@ private: \
 public:
 
 
-<<<<<<< HEAD
-#define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_ENHANCED_CONSTRUCTORS \
-=======
 #define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_ENHANCED_CONSTRUCTORS \
->>>>>>> main
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	NO_API ARaiseTheGameJame2020Character(ARaiseTheGameJame2020Character&&); \
@@ -104,26 +79,11 @@ DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARaiseTheGameJame2020Character); \
 	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ARaiseTheGameJame2020Character)
 
 
-<<<<<<< HEAD
-#define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_PRIVATE_PROPERTY_OFFSET \
-=======
 #define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_PRIVATE_PROPERTY_OFFSET \
->>>>>>> main
 	FORCEINLINE static uint32 __PPO__CameraBoom() { return STRUCT_OFFSET(ARaiseTheGameJame2020Character, CameraBoom); } \
 	FORCEINLINE static uint32 __PPO__FollowCamera() { return STRUCT_OFFSET(ARaiseTheGameJame2020Character, FollowCamera); }
 
 
-<<<<<<< HEAD
-#define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_16_PROLOG
-#define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_GENERATED_BODY_LEGACY \
-PRAGMA_DISABLE_DEPRECATION_WARNINGS \
-public: \
-	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_PRIVATE_PROPERTY_OFFSET \
-	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_SPARSE_DATA \
-	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_RPC_WRAPPERS \
-	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_INCLASS \
-	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_STANDARD_CONSTRUCTORS \
-=======
 #define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_17_PROLOG
 #define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_GENERATED_BODY_LEGACY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
@@ -133,21 +93,10 @@ public: \
 	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_RPC_WRAPPERS \
 	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_INCLASS \
 	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_STANDARD_CONSTRUCTORS \
->>>>>>> main
 public: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
-<<<<<<< HEAD
-#define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_GENERATED_BODY \
-PRAGMA_DISABLE_DEPRECATION_WARNINGS \
-public: \
-	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_PRIVATE_PROPERTY_OFFSET \
-	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_SPARSE_DATA \
-	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_RPC_WRAPPERS_NO_PURE_DECLS \
-	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_INCLASS_NO_PURE_DECLS \
-	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_19_ENHANCED_CONSTRUCTORS \
-=======
 #define RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
@@ -156,7 +105,6 @@ public: \
 	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_RPC_WRAPPERS_NO_PURE_DECLS \
 	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_INCLASS_NO_PURE_DECLS \
 	RaiseTheGameJame2020_Source_RaiseTheGameJame2020_RaiseTheGameJame2020Character_h_20_ENHANCED_CONSTRUCTORS \
->>>>>>> main
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
